dec0d4e64bf7f4ae7c5b230be4cda99e3314f565
Yuval Lande <yuvalande@gmail.com>
2022-12-31 13:20:45 +0200

    implmented overloading not including the bonus

--- wet3/webserver-files/server.c
+++ wet3/webserver-files/server.c
@@ -33,6 +33,10 @@ void getargs(int *port, int *threads, int *conns, int argc, char *argv[])
         exit(1);
     }
     *conns = atoi(argv[3]);
+    if (argc < 5) {
+        fprintf(stderr, "Usage: %s <schedalg>\n", argv[0]);
+        exit(1);
+    }
 }
 
 
@@ -42,10 +46,8 @@ int main(int argc, char *argv[])
     struct sockaddr_in clientaddr;
 
     getargs(&port, &threads, &conns, argc, argv);
-    
-    // 
+    char* shched_alg = argv[4];
     // HW3: Create some threads...
-    //
     thread_pool *tp = threadPoolInit(threads, conns);
 
     listenfd = Open_listenfd(port);
@@ -58,14 +60,44 @@ int main(int argc, char *argv[])
         // Save the relevant info in a buffer and have one of the worker threads 
         // do the work. 
         // 
-        pthread_mutex_lock(&tp->conn_lock);
-        while(tp->buffer_conn.size + tp->num_handled_conn == tp->max_conns)
-            pthread_cond_wait(&tp->conn_cond, &tp->conn_lock);
+        if(strcmp("dt", shched_alg) == 0) {
+            if(tp->buffer_conn.size + tp->num_handled_conn == tp->max_conns) {
+                Close(connfd);
+                continue;
+            }
+            pthread_mutex_lock(&tp->conn_lock);
+            enqueue(&tp->buffer_conn, connfd);
+            pthread_cond_signal(&tp->conn_cond);
+            pthread_mutex_unlock(&tp->conn_lock);
+        }
+        else if(strcmp("block", shched_alg) == 0) {
+            pthread_mutex_lock(&tp->conn_lock);
+            while(tp->buffer_conn.size + tp->num_handled_conn == tp->max_conns)
+                pthread_cond_wait(&tp->conn_cond, &tp->conn_lock);
 
-        // insert conn to back of queue
-        enqueue(&tp->buffer_conn, connfd);
-        pthread_cond_signal(&tp->conn_cond);
-        pthread_mutex_unlock(&tp->conn_lock);
+            // insert conn to back of queue
+            enqueue(&tp->buffer_conn, connfd);
+            pthread_cond_signal(&tp->conn_cond);
+            pthread_mutex_unlock(&tp->conn_lock);
+        }
+        else if(strcmp("dh", shched_alg) == 0) {
+            if(tp->buffer_conn.size + tp->num_handled_conn == tp->max_conns) {
+                pthread_mutex_lock(&tp->conn_lock);
+                conn_t conn_dropped = dequeue(&tp->buffer_conn);
+                enqueue(&tp->buffer_conn, connfd);
+                pthread_mutex_unlock(&tp->conn_lock);
+                Close(conn_dropped);
+                continue;
+            }
+            pthread_mutex_lock(&tp->conn_lock);
+            enqueue(&tp->buffer_conn, connfd);
+            pthread_cond_signal(&tp->conn_cond);
+            pthread_mutex_unlock(&tp->conn_lock);
+        }
+        else {
+            app_error("bad sched_alg");
+            return 1;
+        }
     }
 
     threadPoolDestroy(tp);
