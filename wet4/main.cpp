0709e160b8afcb3781057a0769475f90e1a2dd6d
Yubkush <yuvalande@gmail.com>
2023-01-14 11:08:20 +0000

    tested calloc part2

--- wet4/main.cpp
+++ wet4/main.cpp
@@ -1,4 +1,5 @@
 #include "malloc_2.cpp"
+#include <iostream>
 
 void build_arr(int *arr, size_t size) {
     for (size_t i = 0; i < size; i++) {
@@ -6,12 +7,19 @@ void build_arr(int *arr, size_t size) {
     }
 }
 
-int main() {
-    int *a = (int*)smalloc(sizeof(int) * 10);
-    int *b = (int*)smalloc(sizeof(int) * 20);
-    int *c = (int*)smalloc(0);
-    int *d = (int*)smalloc((size_t)(1e8+1));
+void print_arr(int *arr, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        std::cout << arr[i];
+    }
+    std::cout << std::endl;
+}
 
-    build_arr(a, 10);
-    build_arr(b, 20);
+int main() {
+    int *a = (int*)scalloc(10, sizeof(int));
+    int *b = (int*)scalloc(20, sizeof(int));
+    int *c = (int*)scalloc(0, sizeof(int));
+    int *d = (int*)scalloc((size_t)(1e8+1), sizeof(int));
+
+    print_arr(a, 10);
+    print_arr(b, 20);
 }
