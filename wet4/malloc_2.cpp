2964f9e718efe8924584898e775061e9f90afc96
Yuval Lande <yuvalande@gmail.com>
2023-01-14 13:29:04 +0200

    implemented sfree, srealloc part2

--- wet4/malloc_2.cpp
+++ wet4/malloc_2.cpp
@@ -4,6 +4,7 @@
 #define SYSCALL_FAIL -1
 
 using std::memset;
+using std::memmove;
 
 typedef struct MallocMetadata {
     size_t size; // include metadata_t size
@@ -43,11 +44,37 @@ void* smalloc(size_t size) {
     return (char*)tmp + sizeof(metadata_t);
 }
 
-void* scalloc(size_t num, size_t size){
+void* scalloc(size_t num, size_t size) {
     void *addr = smalloc(size * num);
     if(addr == nullptr) {
-        return nullptr;
+        return NULL;
     }
     memset(addr, 0, size * num);
     return addr;
+}
+
+void sfree(void* p) {
+    if(p == nullptr)
+        return;
+    metadata_t *p_meta = (metadata_t*)p - 1;
+    p_meta->is_free = true;
+}
+
+void* srealloc(void* oldp, size_t size) {
+    size_t real_size = size + sizeof(metadata_t);
+    if(size == 0 || real_size > MAX_SIZE){
+        return NULL;
+    }
+    if(oldp == nullptr){
+        return smalloc(size);
+    }
+    metadata_t *p_meta = (metadata_t*)oldp - 1;
+    if(p_meta->size < real_size){
+        return oldp;
+    }
+    void* newp = smalloc(real_size);
+    if(newp == nullptr){return NULL;}
+    memmove(newp, oldp, size);
+    sfree(oldp);
+    return newp;
 }
