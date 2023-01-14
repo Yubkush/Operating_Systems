ff51b1593dd76170fb9e14d8e587f66fbf67b4e9
Yuval Lande <yuvalande@gmail.com>
2023-01-14 12:43:28 +0200

    changed smalloc implementation (no stl allowed)

--- wet4/malloc_2.cpp
+++ wet4/malloc_2.cpp
@@ -1,35 +1,45 @@
 #include <unistd.h>
-#include <list>
 #define MAX_SIZE (size_t)1e8
 #define SYSCALL_FAIL -1
 
-using std::list;
 
 typedef struct MallocMetadata {
     size_t size; // include metadata_t size
     bool is_free;
-    void* meta_address; // address of metadata
-    void* data_address; // address of allocated data
+    MallocMetadata* next;
+    MallocMetadata* prev;
 } metadata_t;
 
-list<metadata_t> free_list;
+static metadata_t list_head = {0, false, nullptr, nullptr};
 
 void* smalloc(size_t size) {
     size_t real_size = size + sizeof(metadata_t);
     if(size == 0 || real_size > MAX_SIZE){
         return NULL;
     }
-    for(metadata_t& block: free_list){
-        if(block.is_free && block.size >= real_size){
-            block.is_free = false;
-            return block.data_address;
-        }   
+    metadata_t *tmp = list_head.next;
+    metadata_t *last;
+    while(tmp != nullptr){
+        if(tmp->is_free && tmp->size >= real_size){
+            tmp->is_free = false;
+            return (char*)tmp + sizeof(metadata_t);
+        }
+        if(tmp->next == nullptr){last = tmp;}
+        tmp = tmp->next; 
     }
-    void *meta_addr = sbrk(real_size);
-    if(*(ssize_t*)brk == SYSCALL_FAIL) {
+    void *res = sbrk(real_size);
+    if(*(ssize_t*)res == SYSCALL_FAIL) {
         return NULL;
     }
-    void *data_addr = (char*)meta_addr + sizeof(metadata_t);
-    free_list.push_back((metadata_t){real_size, false, meta_addr, data_addr});
-    return data_addr;
+    tmp = (metadata_t*)res;
+    tmp->size = real_size;
+    tmp->is_free = false;
+    tmp->next = nullptr;
+    tmp->prev = last;
+    last->next = tmp;
+    return (char*)tmp + sizeof(metadata_t);
+}
+
+void* scalloc(size_t num, size_t size){
+    
 }
