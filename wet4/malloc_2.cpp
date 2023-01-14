58978d828820292208c221d78f16c5d8c8fd4671
Yuval Lande <yuvalande@gmail.com>
2023-01-14 15:22:32 +0200

    implemented stats functions

--- wet4/malloc_2.cpp
+++ wet4/malloc_2.cpp
@@ -13,6 +13,17 @@ typedef struct MallocMetadata {
     MallocMetadata* prev;
 } metadata_t;
 
+typedef struct Stats {
+    size_t num_free_blocks;
+    size_t num_free_bytes;
+    size_t num_allocated_blocks;
+    size_t num_allocated_bytes;
+    // size_t num_meta_data_bytes = num_allocated_blocks * sizeof(metadata_t)
+    // size_t size_meta_data = sizeof(metadata_t)
+} stats_t;
+
+stats_t stats = {0, 0, 0, 0};
+
 static metadata_t list_head = {0, false, nullptr, nullptr};
 
 void* smalloc(size_t size) {
@@ -25,6 +36,8 @@ void* smalloc(size_t size) {
     while(tmp != nullptr){
         if(tmp->is_free && tmp->size >= real_size){
             tmp->is_free = false;
+            stats.num_free_blocks--;
+            stats.num_free_bytes -= tmp->size - sizeof(metadata_t);
             return (char*)tmp + sizeof(metadata_t);
         }
         if(tmp->next == nullptr){last = tmp;}
@@ -41,6 +54,8 @@ void* smalloc(size_t size) {
     if(list_head.next == nullptr){last = &list_head;} // if this is the first node
     tmp->prev = last;
     last->next = tmp;
+    stats.num_allocated_blocks++;
+    stats.num_allocated_bytes += size;
     return (char*)tmp + sizeof(metadata_t);
 }
 
@@ -54,10 +69,12 @@ void* scalloc(size_t num, size_t size) {
 }
 
 void sfree(void* p) {
-    if(p == nullptr)
-        return;
     metadata_t *p_meta = (metadata_t*)p - 1;
+    if(p == nullptr || p_meta->is_free == true)
+        return;
     p_meta->is_free = true;
+    stats.num_free_blocks++;
+    stats.num_free_bytes += p_meta->size - sizeof(metadata_t);
 }
 
 void* srealloc(void* oldp, size_t size) {
@@ -78,3 +95,10 @@ void* srealloc(void* oldp, size_t size) {
     sfree(oldp);
     return newp;
 }
+
+size_t _num_free_blocks(){ return stats.num_free_blocks; }
+size_t _num_free_bytes(){ return stats.num_free_bytes; }
+size_t _num_allocated_blocks(){ return stats.num_allocated_blocks; }
+size_t _num_allocated_bytes(){ return stats.num_allocated_bytes; }
+size_t _num_meta_data_bytes(){ return stats.num_allocated_blocks * sizeof(metadata_t); }
+size_t _size_meta_data(){ return sizeof(metadata_t); }
